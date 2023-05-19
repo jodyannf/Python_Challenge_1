55f92096709add3062de2bcb485e36fce2db7f93
jodyannf <129745806+jodyannf@users.noreply.github.com>
2023-05-19 18:06:31 +0100

    pybank completed

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -12,14 +12,40 @@ with open(Budget_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     print(csvreader)
-#Read the header row first 
-    #csv_header = next(csvreader)
-    #print(f" CSV Header: {csv_header}")
+    #Read the header row first 
+    csv_header = next(csvreader)
+    print(f" CSV Header: {csv_header}")
     print("Financial Analysis")
 
+    counter = 0 
+    total = 0
+    previous_profit = 1088983
+    total_change = 0
+    greatest_change = 0
+    smallest_change = 0
+
+
     for row in csvreader:
-    #print(row)
-     lines= len(list(csvreader))
-     print("Total Months:" + str(lines))
+        print (row[1])
+        profit = int(row [1])
+        month = row [0]
 
+        counter = counter + 1
+        total = total + profit
+        Change = profit - previous_profit 
+        previous_profit = profit
+        total_change = total_change + Change
 
+        if Change > greatest_change :
+            greatest_change = Change
+            greatest_month = month 
+
+        if Change < greatest_change :
+            smallest_change = Change 
+            smallest_month = month
+
+    print("Total Months:" +  str(counter))
+    print ("Total: $" + str(total))
+    print (f"Average Change: ${round (total_change/(counter-1), 2)}")
+    print (f"Greatest Increase in profits: {greatest_month} ${greatest_change}")
+    print (f"Greatest Decrease in profits: {smallest_month} ${smallest_change}")
